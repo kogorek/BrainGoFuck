cd40525daf663e67f97ebced3d884f35862e6b18
Igor Lihachev <kogorek@mail.ru>
2018-08-31 12:42:15 +0300

    Add readfile and print source functions

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-type BrainGoFuck struct {
+import "io/ioutil"
+import "errors"
 
+type BrainGoFuck struct {
+	source string
 }
 
-func NewBrainGoFuck () {
-	
+func (this *BrainGoFuck) ReadFile(fileName string) error {
+	b, err := ioutil.ReadFile(fileName)
+    if err != nil {
+        return errors.New("Cannot open file")
+    }
+    this.source = string(b)
+    return nil
+}
+
+func (this BrainGoFuck) PrintSource() {
+	println(this.source[0])
+}
+
+func NewBrainGoFuck () BrainGoFuck {
+	return BrainGoFuck{""}
 }
 
 func main() {
-	
+	var interpretator BrainGoFuck = NewBrainGoFuck() 
+	interpretator.ReadFile("hello.bf")
+	interpretator.PrintSource()
 }
